922dfe988b922e59c9076db6427a69f18000e99e
Mircea Dan <mircea_dan97@yahoo.com>
2018-06-24 13:20:40 +0300

    initial commit, psutil installed and setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='monitoru',
+      version='0.1',
+      description="Client side monitoring system for your pc's metrics",
+      url='https://github.com/HmirceaD/monitoru.git',
+      license='Apache License 2.0',
+      author='Mircea Dan',
+      author_email='mircea_dan97@yahoo.com',
+      packages=find_packages())
